d1df6ed9eb95c42e46ade728af2da153d34527a4
William Ritson <william.ritson@gmail.com>
2018-02-04 17:43:42 -0800

    Added tables for decks and collections.

--- sql/makeDB.sql
+++ sql/makeDB.sql
@@ -1,4 +1,3 @@
-DROP SCHEMA CCG CASCADE; 
 CREATE SCHEMA CCG;
 
 CREATE TABLE CCG.Account (
@@ -6,7 +5,32 @@ CREATE TABLE CCG.Account (
     username            VARCHAR(30) UNIQUE,
     email               VARCHAR(254) UNIQUE,
     emailVerified       BOOLEAN DEFAULT false,
-    skillLevel          INTEGER DEFAULT 800,
+    skillLevel          SMALLINT DEFAULT 800,
     password            VARCHAR(256),
     salt                VARCHAR(32)
+);
+
+CREATE TABLE CCG.CollectionRecord (
+    accountID           INTEGER,
+    cardID              VARCHAR(30),
+    repeats             SMALLINT,
+    FOREIGN KEY (accountID) REFERENCES CCG.Account(accountID),
+    Primary Key (accountID, cardID)
+);
+
+CREATE TABLE CCG.Deck (
+    deckID              SERIAL PRIMARY KEY,
+    accountID           INTEGER,
+    name                VARCHAR(30),
+    avatar              VARCHAR(30),
+    shared              BOOLEAN DEFAULT false,
+    FOREIGN KEY (accountID) REFERENCES CCG.Account(accountID)
+);
+
+CREATE TABLE CCG.DeckRecord (
+    deckID              INTEGER,
+    cardID              VARCHAR(30),
+    repeats             SMALLINT,
+    FOREIGN KEY (deckID) REFERENCES CCG.Deck(deckID),
+    Primary Key (deckID, cardID)
 );
