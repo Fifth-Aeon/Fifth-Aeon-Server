c9f9efe2d5a6ce9f027b7b6bfd637cecb40e8cf7
William Ritson <william.ritson@gmail.com>
2018-02-04 17:43:28 -0800

    Create DB on start if not exist.

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,22 @@
 import { Pool, Client } from 'pg';
-
+import * as fs from 'fs';
+import { DeckList } from './game_model/deckList';
+import { saveDeck } from './models/cards';
 export const db = new Pool();
 
+async function startup() {
+  const existQuery = await db.query(`SELECT EXISTS (
+    SELECT 1
+    FROM   information_schema.tables 
+    WHERE  table_schema = 'ccg'
+    );`);
+    if (!existQuery.rows[0].exists) {
+      console.warn('No CCG Schema detected. Creating now.');
+      fs.readFile('./sql/makeDB.sql', 'utf8', function(err, sql) {
+        if (err) throw err;
+        db.query(sql);
+      });
+    }
+}
 
-  /*
-    PGUSER=postgres \
-    PGDATABASE=ccg \
-    npm start
-    */
+startup();
