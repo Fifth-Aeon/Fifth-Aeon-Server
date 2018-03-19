459c570bc3def4d85696465a106f8fc12d408ed4
William Ritson <william.ritson@gmail.com>
2018-03-19 00:48:41 -0700

    Updated avaliblity routes
    Better case insensitivity

--- src/routes/avalibility.ts
+++ src/routes/avalibility.ts
@@ -5,21 +5,52 @@ import { db } from "../db";
 import { NextFunction, Request, Response } from "express";
 
 
-function checkAvalibility(table: string, attribute: string) {
+/**
+ * Checks if a certain table has a row with the given attribute.
+ * This is useful so the client can immediatly provide feedback on
+ * whether a certain unique key is avalible.
+ *
+ * @param {string} table The table whose attribute we are checking
+ * @param {string} attribute The key of the table
+ * @param {boolean} caseInsensitive Whether the search should ignore capitiization
+ * @returns {function(Request, Response, NextFunction): void} Route handler that checks the attribute
+ */
+const checkAvalibility = (table: string, attribute: string, caseInsensitive: boolean) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const query = await db.query(`SELECT ${attribute} FROM CCG.${table} WHERE ${attribute}=$1`, [req.params[attribute]]);
+            const sql = caseInsensitive ?
+                `SELECT ${attribute} FROM CCG.${table} WHERE LOWER(${attribute})=LOWER($1)` :
+                `SELECT ${attribute} FROM CCG.${table} WHERE ${attribute}=$1`;
+            const query = await db.query(sql, [req.params[attribute]]);
             res.json({
-                availbile: query.rowCount === 0
+                available: query.rowCount === 0
             });
         } catch (err) {
             next(err);
+            return;
         }
     };
-}
+};
 
-router.get('/email/:email', checkAvalibility('Account', 'email'));
-router.get('/username/:username', checkAvalibility('Account', 'username'));
+router.get('/emailorpassword/:nameOrPass', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const nameOrPass = req.params.nameOrPass.toString();
+        const query = await db.query(`
+            SELECT accountID 
+            FROM CCG.Account 
+            WHERE LOWER(username) = LOWER($1)
+               OR LOWER(email)    = LOWER($2)`, [nameOrPass, nameOrPass.toLowerCase()]);
+        res.json({
+            available: query.rowCount === 0
+        });
+    } catch (err) {
+        next(err);
+        return;
+    }
+});
+
+router.get('/email/:email', checkAvalibility('Account', 'email', true));
+router.get('/username/:username', checkAvalibility('Account', 'username', true));
 
 
 export const avalibilityRoutes = router;
